b1f81d8a8dfe781761351a90e8ca1f6a3d03ce3a
Malu356 <luiza.maria.ml48@gmail.com>
2026-04-27 18:16:15 -0300

    Atualizar partidas

--- js/config.js
+++ js/config.js
@@ -32,206 +32,156 @@ const EVENT_CONFIG = {
     "148107692": {nick: "Ragnar"},
     "150430172": {nick: "beta"},
     "326930570": {nick: "Ze do taxão"},
-    "489654421": {nick: "Power Ranger"}
+    "489654421": {nick: "Power Ranger"},
+    "1824757749": {nick: "Khzan"},
+    "203009194": {nick: "Squash"},
+    "86724917": {nick: "Siri"}
   },
 
   matches: [
     {
-      matchId: 8775764161,
-      teamA: "MODO PRIME ESPORTS",
-      teamB: "Bonde dos Careca",
+      matchId: 8788019524,
+      teamA: "VAMPIROS SUGADORES",
+      teamB: "Corvus Nox",
       mmrType: {
-        "116369675": "normal",
         "174409523": "normal",
-        "22342606": "normal",
-        "115291593": "normal",
-        "94124488": "normal",
-        "464672251": "normal",
+        "369402582": "normal",
+        "1605370486": "normal",
+        "1056737735": "normal",
+        "96799691": "normal",
+        "1824757749": "normal",
         "100758466": "normal",
-        "1417879812": "normal",
-        "1057024578": "normal",
-        "1270256342": "normal"
+        "108965355": "normal",
+        "1279187093": "normal",
+        "122390839": "normal"
       }
     },
 
     {
-    matchId: 8775809590,
-    teamA: "TIME IMORAIS",
-    teamB: "TEAM MAFIA",
-    mmrType: {
-      "116369675": "normal",
-      "1057024578": "normal",
-      "919217173": "normal",
-      "115291593": "normal",
-      "94124488": "normal",
-      "174409523": "normal",
-      "1417879812": "normal",
-      "1270256342": "normal",
-      "22342606": "normal",
-      "464672251": "normal"
-    }
-  },
+      matchId: 8786666549,
+      teamA: "Discolados",
+      teamB: "EZ MONEY",
+      mmrType: {
+        "464672251": "normal",
+        "174409523": "normal",
+        "1057024578": "normal",
+        "919217173": "normal",
+        "96799691": "normal",
+        "369402582": "normal",
+        "100758466": "normal",
+        "103780867": "normal",
+        "94124488": "normal",
+        "98860863": "normal"
+      }
+    },
 
-  {
-  matchId: 8777194998,
-  teamA: "Divonicos",
-  teamB: "MODO PRIME ESPORTS",
-  mmrType: {
-    "369402582": "normal",
-    "100758466": "normal",
-    "337118609": "normal",
-    "108965355": "normal",
-    "1417879812": "normal",
-    "116369675": "normal",
-    "94124488": "normal",
-    "98860863": "normal",
-    "115291593": "normal",
-    "779968891": "normal"
-  }
-},
+    {
+      matchId: 8786596979,
+      teamA: "Discolados",
+      teamB: "EZ MONEY",
+      mmrType: {
+        "174409523": "normal",
+        "464672251": "normal",
+        "919217173": "normal",
+        "1057024578": "normal",
+        "96799691": "normal",
+        "369402582": "normal",
+        "100758466": "normal",
+        "94124488": "normal",
+        "98860863": "normal",
+        "103780867": "normal"
+      }
+    },
 
-{
-  matchId: 8777223830,
-  teamA: "Corvus Nox",
-  teamB: "EZ MONEY",
-  mmrType: {
-    "1057024578": "normal",
-    "100758466": "normal",
-    "174409523": "normal",
-    "108965355": "normal",
-    "779968891": "normal",
-    "116369675": "normal",
-    "98860863": "normal",
-    "1417879812": "normal",
-    "122390839": "normal",
-    "115291593": "normal"
-  }
-},
+    {
+      matchId: 8786508115,
+      teamA: "Discolados",
+      teamB: "Taxa de Elite",
+      mmrType: {
+        "464672251": "normal",
+        "919217173": "normal",
+        "94124488": "normal",
+        "174409523": "normal",
+        "96799691": "normal",
+        "369402582": "normal",
+        "489654421": "normal",
+        "326930570": "normal",
+        "1063834463": "normal",
+        "425171359": "normal"
+      }
+    },
 
-{
-  matchId: 8777269257,
-  teamA: "TEAM MAFIA",
-  teamB: "EZ MONEY",
-  mmrType: {
-    "1417879812": "normal",
-    "1057024578": "normal",
-    "174409523": "normal",
-    "337118609": "normal",
-    "100758466": "normal",
-    "464672251": "normal",
-    "116369675": "normal",
-    "94124488": "normal",
-    "115291593": "normal",
-    "122390839": "normal"
-  }
-},
+    {
+      matchId: 8786455685,
+      teamA: "EZ MONEY",
+      teamB: "RoneyCampos Fan Club",
+      mmrType: {
+        "878213636": "normal",
+        "464672251": "normal",
+        "98860863": "normal",
+        "203009194": "double",
+        "326930570": "normal",
+        "1605370486": "normal",
+        "100758466": "normal",
+        "213070699": "normal",
+        "1063834463": "normal",
+        "96799691": "normal"
+      }
+    },
 
-{
-  matchId: 8777311672,
-  teamA: "EZ MONEY",
-  teamB: "Bonde dos Careca",
-  mmrType: {
-    "98860863": "normal",
-    "116369675": "normal",
-    "1417879812": "normal",
-    "94124488": "normal",
-    "115291593": "normal",
-    "464672251": "normal",
-    "100758466": "normal",
-    "337118609": "normal",
-    "22342606": "normal",
-    "174409523": "normal"
-  }
-},
+    {
+      matchId: 8786415006,
+      teamA: "EZ MONEY",
+      teamB: "Divonicos",
+      mmrType: {
+        "98860863": "normal",
+        "94124488": "normal",
+        "86724917": "normal",
+        "464672251": "normal",
+        "1057024578": "normal",
+        "369402582": "normal",
+        "100758466": "normal",
+        "1279187093": "normal",
+        "203009194": "double",
+        "425171359": "normal"
+      }
+    },
 
-{
-  matchId: 8777358510,
-  teamA: "EZ MONEY",
-  teamB: "Temidos",
-  mmrType: {
-    "116369675": "normal",
-    "98860863": "normal",
-    "464672251": "normal",
-    "1417879812": "normal",
-    "115291593": "normal",
-    "174409523": "normal",
-    "1057024578": "normal",
-    "100758466": "normal",
-    "1056737735": "normal",
-    "22342606": "normal"
-  }
-},
+    {
+      matchId: 8785128528,
+      teamA: "Bonde dos careca",
+      teamB: "falso +4",
+      mmrType: {
+        "174409523": "normal",
+        "1270256342": "normal",
+        "103780867": "normal",
+        "100758466": "normal",
+        "108965355": "normal",
+        "96799691": "normal",
+        "464672251": "normal",
+        "1057024578": "normal",
+        "326930570": "normal",
+        "94124488": "normal"
+      }
+    },
 
-{
-  matchId: 8778594323,
-  teamA: "TIME JUGGERNAUT",
-  teamB: "EZ MONEY",
-  mmrType: {
-    "174409523": "normal",
-    "22342606": "normal",
-    "1417879812": "normal",
-    "169011821": "normal",
-    "369402582": "normal",
-    "98860863": "normal",
-    "100758466": "normal",
-    "251002233": "normal",
-    "108965355": "normal",
-    "878213636": "normal"
-  }
-},
+    {
+      matchId: 8785064421,
+      teamA: "Bonde dos Careca",
+      teamB: "EZ MONEY",
+      mmrType: {
+        "174409523": "normal",
+        "100758466": "normal",
+        "1270256342": "normal",
+        "108965355": "normal",
+        "96799691": "normal",
+        "1057024578": "normal",
+        "489654421": "normal",
+        "464672251": "normal",
+        "404990590": "normal",
+        "98860863": "normal"
+      }
+    },
 
-{
-  matchId: 8778663537,
-  teamA: "Corvus Nox",
-  teamB: "TEAM MAFIA",
-  mmrType: {
-    "369402582": "normal",
-    "100758466": "normal",
-    "22342606": "normal",
-    "1279187093": "normal",
-    "103780867": "normal",
-    "148107692": "normal",
-    "174409523": "normal",
-    "94124488": "normal",
-    "1417879812": "normal",
-    "150430172": "normal"
-  }
-},
-
-{
-  matchId: 8778629799,
-  teamA: "EZ MONEY",
-  teamB: "Divonicos",
-  mmrType: {
-    "98860863": "normal",
-    "251002233": "normal",
-    "103780867": "normal",
-    "108965355": "normal",
-    "326930570": "normal",
-    "174409523": "normal",
-    "22342606": "normal",
-    "369402582": "normal",
-    "489654421": "normal",
-    "1417879812": "normal"
-  }
-},
-
-{
-  matchId: 8778702300,
-  teamA: "TEAM MAFIA",
-  teamB: "VAMPIROS SUGADORES",
-  mmrType: {
-    "22342606": "normal",
-    "94124488": "normal",
-    "174409523": "normal",
-    "108965355": "normal",
-    "100758466": "normal",
-    "369402582": "normal",
-    "150430172": "normal",
-    "103780867": "normal",
-    "1417879812": "normal",
-    "337118609": "normal"
-  }
-}
   ]
 };
